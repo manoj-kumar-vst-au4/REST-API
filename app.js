68dbbe339e2ed4fc92857b3b5b39d76ea8daa8ce
manoj-kumar-vst-au4 <mauryamanoj513@gmail.com>
2020-05-18 02:50:45 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./database");
 const app = express();
-const PORT = 4000 || process.env.PORT
+const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
